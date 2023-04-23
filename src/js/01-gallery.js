6933e4587007676fe46785b3377c1c4609fb5ed1
YuliiaKryviakina <117831702+YuliiaKryviakina@users.noreply.github.com>
2023-04-24 00:38:39 +0300

    task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,55 +1,34 @@
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
-const galleryCardList = createGalleryItems(galleryItems);
+const galleryMarkUp = createGalleryItems(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkUp);
 
 function createGalleryItems(galleryItems) {
     return galleryItems
-        .map(({ preview, original, description }) => {
-            return `<li class="gallery__item"><a class="gallery__link" ${original}>
+        .map(({ original, preview, description }) => {
+            return `<li class="gallery__item"><a class="gallery__item" href="${original}">
         <img
         class="gallery__image"
         src="${preview}"
-        data-source="${original}"
         alt="${description}"
         />
         </a>
         </li>`;
         })
         .join('');
-}
+};
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryCardList);
-galleryContainer.addEventListener('click', selectGalleryEl);
+const lightbox = new SimpleLightbox(".gallery a", {
+    captions: true,
+    captionsData: 'alt',
+    captionPosition: 'bottom',
+    captionDelay: 250,
+})
 
-function selectGalleryEl(evt) {
-    const galleryImage = evt.target.classList.contains('gallery__image');
-    if (!galleryImage) {
-        return;
-    }
-
-    const instance = basicLightbox.create(
-        `<img src="${evt.target.dataset.source}" width="900" height="700">`,
-        {
-            onShow: () => {
-                window.addEventListener('keydown', onKeydownEsc);
-            },
-            onClose: () => {
-                window.removeEventListener('keydown', onKeydownEsc)
-            },
-        },
-    );
-
-    const onKeydownEsc = evt => {
-        console.log(evt.code);
-        if (evt.code === 'Escape') {
-            instance.close();
-        }
-    };
-    instance.show();
-}
