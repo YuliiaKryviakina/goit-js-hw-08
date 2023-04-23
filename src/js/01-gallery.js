40530ff46d2a301c8f2e55a02ae48ffcc71d7813
YuliiaKryviakina <117831702+YuliiaKryviakina@users.noreply.github.com>
2023-04-23 23:54:44 +0300

    add script

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,51 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryCardList = createGalleryItems(galleryItems);
+
+function createGalleryItems(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `<li class="gallery__item"><a class="gallery__link" ${original}>
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+        </a>
+        </li>`;
+        })
+        .join('');
+}
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryCardList);
+galleryContainer.addEventListener('click', selectGalleryEl);
+
+function selectGalleryEl(evt) {
+    const galleryImage = evt.target.classList.contains('gallery__image');
+    if (!galleryImage) {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+        `<img src="${evt.target.dataset.source}" width="900" height="700">`,
+        {
+            onShow: () => {
+                window.addEventListener('keydown', onKeydownEsc);
+            },
+            onClose: () => {
+                window.removeEventListener('keydown', onKeydownEsc)
+            },
+        },
+    );
+
+    const onKeydownEsc = evt => {
+        console.log(evt.code);
+        if (evt.code === 'Escape') {
+            instance.close();
+        }
+    };
+    instance.show();
+}
