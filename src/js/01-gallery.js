6aea5dd6da4567364ebe955e5b71293d32f1714e
YuliiaKryviakina <117831702+YuliiaKryviakina@users.noreply.github.com>
2023-04-24 00:02:54 +0300

    add slb

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,6 +1,9 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+console.log(galleryItems);
 
 const galleryContainer = document.querySelector('.gallery');
 const galleryCardList = createGalleryItems(galleryItems);
