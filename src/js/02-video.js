207fd3cdb704c31451dee07b85b5917fd63552f8
YuliiaKryviakina <117831702+YuliiaKryviakina@users.noreply.github.com>
2023-04-24 21:38:59 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle (evt => {
+    localStorage.setItem('videoplayer - current - time', evt.seconds);
+}, 1000)
+);
+
+player
+    .setCurrentTime(localStorage.getItem('videoplayer - current - time'))
+    .catch(function (error) {
+        console.error(error)
+    });
